c9daac1527df80ff3b4e27cc928ef430043d9050
nhope <16958187+nhope123@users.noreply.github.com>
2020-09-05 19:17:43 -0400

    Updates

---
+++ precache-manifest.3e55e2b71552500ed879b5f9f33f19ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe3ad90c74a178b20b5a9d7866c9fe9",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "cff825de50c727c157b1",
+    "url": "/calculator/static/css/main.5ea09f4f.chunk.css"
+  },
+  {
+    "revision": "b920011cc3d0ff03f5ba",
+    "url": "/calculator/static/js/2.56395151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.56395151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff825de50c727c157b1",
+    "url": "/calculator/static/js/main.72daba08.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
